ccf093aa784648397f01ab7d3880dcfde7397035
Alwin J. Kowattu <138890707+alwinj360@users.noreply.github.com>
2024-08-02 22:30:51 +0000

    Edited  'pom.xml' and 'App.java'..

--- SqlServerSample/src/main/java/com/sqlsamples/App.java
+++ SqlServerSample/src/main/java/com/sqlsamples/App.java
@@ -1,13 +1,24 @@
 package com.sqlsamples;
 
-/**
- * Hello world!
- *
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // Replace placeholders with your database info.
+        String connectionUrl = "jdbc:sqlserver://<your_server.database.windows.net>:<your_database_port>;databaseName=master;encrypt=true;trustServerCertificate=true;user=<your_username>;password=<your_password>";
+
+        try {
+            // Load SQL Server JDBC driver and establish connection.
+            System.out.print("Connecting to SQL Server ... ");
+            try (Connection connection = DriverManager.getConnection(connectionUrl)) {
+                System.out.println("Done.");
+            }
+        } catch (Exception e) {
+            System.out.println();
+            e.printStackTrace();
+        }
     }
 }
